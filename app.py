1d2857384a75aeab32758d974bc04a8ec12bcbf9
jnazare <jules.nazare@gmail.com>
2015-08-24 16:11:28 -0400

    question template

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ def course_info(course_id):
 
 @app.route('/course/<course_id>/info/<question_id>')
 def question_info(course_id, question_id):
-	print 'here'
 	return render_template('question_info.html')
 
+@app.route('/course/<course_id>/question/<question_id>')
+def question_response(course_id, question_id):
+	return render_template('question_response.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
