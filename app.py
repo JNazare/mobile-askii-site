771330d3d2332582444b8ed0d523d9d8123c5586
jnazare <jules.nazare@gmail.com>
2015-08-23 23:44:06 -0400

    templating

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
 
+@app.route('/course/<course_id>')
+def course_info(course_id):
+    return render_template('course_description.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
